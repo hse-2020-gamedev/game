651cadf1103134eae0b85e72f03a6f7035737afd
Olga Kunyavskaya <OChe24912@gmail.com>
2020-04-24 19:25:24 +0300

    push ball by arrow

--- Assets/StrokeManager.cs
+++ Assets/StrokeManager.cs
@@ -8,9 +8,10 @@ public class StrokeManager : MonoBehaviour
     void Start()
     {
         FindPlayerBall();
-        
     }
 
+    public float StrokeAngle {get; protected set;}
+    
     Rigidbody playerBallRB;
 
     bool doWhack = false;
@@ -29,9 +30,11 @@ public class StrokeManager : MonoBehaviour
     }
 
     private void Update() {
-        if (Input.GetKeyUp(KeyCode.Space)) {
+        if (Input.GetButton("Fire")) {
             doWhack = true;
         }
+
+        StrokeAngle += Input.GetAxis("Horizontal") * 100f * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -43,9 +46,9 @@ public class StrokeManager : MonoBehaviour
 
         if (doWhack) {
              doWhack = false;    
-             Vector3 forceVec = new Vector3(0, 0, 1f);
+             Vector3 forceVec = new Vector3(0, 0, -0.5f);
 
-             playerBallRB.AddForce(forceVec, ForceMode.Impulse);
+             playerBallRB.AddForce(Quaternion.Euler(0, StrokeAngle, 0) * forceVec, ForceMode.Impulse);
         }     
     }
 }
