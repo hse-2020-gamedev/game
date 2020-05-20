2ca23e0f5d9becd35018cdaf5695e27a99d8b723
Olga Kunyavskaya <OChe24912@gmail.com>
2020-05-20 14:35:47 +0300

    cam position overview mode

---
+++ Assets/Scripts/CameraPositionManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositionManager : MonoBehaviour
+{
+    void SetCameraOverview() {
+        GameObject mainCam = GameObject.Find("Main Camera");
+        GameObject levelPlane = GameObject.Find("level0").transform.Find("Plane").gameObject;
+        var PlaneBounds = levelPlane.GetComponent<Renderer>().bounds;
+        var max_len = Math.Max(PlaneBounds.size[0], PlaneBounds.size[2])/2;
+        var FieldOfView = (Camera.main.fieldOfView/2)*Mathf.PI/180;
+        var ycam = max_len*Mathf.Cos(FieldOfView)/Mathf.Sin(FieldOfView) * 1.1f;
+        mainCam.transform.position = new Vector3((PlaneBounds.min[0] + PlaneBounds.max[0])/2, ycam, (PlaneBounds.min[2] + PlaneBounds.max[2])/2);
+        mainCam.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        SetCameraOverview();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
