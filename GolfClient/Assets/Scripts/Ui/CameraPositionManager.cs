0af59cd82247af1eb5a36295b885db152c4109de
Olga Kunyavskaya <OChe24912@gmail.com>
2020-05-23 10:11:32 +0300

    update level0 for materials

--- GolfClient/Assets/Scripts/Ui/CameraPositionManager.cs
+++ GolfClient/Assets/Scripts/Ui/CameraPositionManager.cs
@@ -8,7 +8,7 @@ public class CameraPositionManager {
    
     void SetCameraOverview() {
         GameObject mainCam = GameObject.Find("Main Camera");
-        GameObject levelPlane = GameObject.Find("level0").transform.Find("Plane").gameObject;
+        GameObject levelPlane = GameObject.Find("level0").transform.Find("Wall").gameObject;
         var PlaneBounds = levelPlane.GetComponent<Renderer>().bounds;
         var max_len = Math.Max(PlaneBounds.size[0], PlaneBounds.size[2])/2;
         var FieldOfView = (Camera.main.fieldOfView/2)*Mathf.PI/180;
