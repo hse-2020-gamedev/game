0ae022bea5e6df3b935e5f68c93d2af1e811657b
Olga Kunyavskaya <OChe24912@gmail.com>
2020-06-07 21:13:02 +0300

    play -> choose level

--- GolfClient/Assets/Scenes/MenuBehaviourScript.cs
+++ GolfClient/Assets/Scenes/MenuBehaviourScript.cs
@@ -9,17 +9,20 @@ public class MenuBehaviourScript : MonoBehaviour
     public Button StartButon;
     public Button QuitButton;
     public string GameSceneName;
-
+    public GameObject MainPanel;
+    public GameObject ChooseLevelPanel;
     // Start is called before the first frame update
     void Start()
     {
         StartButon.onClick.AddListener(OnStartButtonClick);
     }
 
-    void OnStartButtonClick()
-    {
-        Debug.Log("start clicked");
-        SceneManager.LoadScene(GameSceneName);
+    void OnStartButtonClick() {
+        MainPanel.SetActive(false);
+        ChooseLevelPanel.SetActive(true);
+        
+        //Debug.Log("start clicked");
+        //SceneManager.LoadScene(GameSceneName);
     }
 
     void OnQuitButotnClick()
