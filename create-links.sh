0666d23d7215acdb1e22415d0c3d36204158690b
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2020-06-23 18:29:27 +0300

    FOOBAR2

--- create-links.sh
+++ create-links.sh
@@ -10,5 +10,6 @@ trap 'err_report $LINENO' ERR
 pushd "$(dirname "$0")"
 SOLUTION_DIR="$(pwd)"
 echo "SOLUTION_DIR: $SOLUTION_DIR"
-ln -sfn "$SOLUTION_DIR/GolfClient/Assets" "$SOLUTION_DIR/GolfServer/Assets/ClientAssets"
+ln -sfn "$SOLUTION_DIR/GolfClient/Assets/Scripts/Shared" "$SOLUTION_DIR/GolfServer/Assets/Scripts/Shared"
+ln -sfn "$SOLUTION_DIR/GolfClient/Assets/Scenes/Levels" "$SOLUTION_DIR/GolfServer/Assets/Scenes/Levels"
 echo "OK"
